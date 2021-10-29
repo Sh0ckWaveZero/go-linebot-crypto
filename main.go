1f5ad898cf3f7dc18fa259b230551cac23224db3
Lisa Lalisa <lalisa@Lalisa-Air.local>
2021-10-29 23:29:58 +0700

    refactor: 💡 break code into functions

--- main.go
+++ main.go
@@ -10,71 +10,82 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
+var bot *linebot.Client
+
 func main() {
+
+	// Connect to Line Bot
+	ConnectLinebot()
+
+	// Setup HTTP Server for receiving requests from LINE platform
+	http.HandleFunc("/webhook", handleWebHook)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "4325"
+		log.Printf("defaulting to port %s", port)
+	}
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Connect to Line Bot
+func ConnectLinebot() {
 	log.Print("🚀 starting server...")
 
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	bot, err := linebot.New(
+	bot, err = linebot.New(
 		os.Getenv("CHANNEL_SECRET"),
 		os.Getenv("CHANNEL_TOKEN"),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	// Setup HTTP Server for receiving requests from LINE platform
-	http.HandleFunc("/webhook", func(w http.ResponseWriter, req *http.Request) {
-		events, err := bot.ParseRequest(req)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				w.WriteHeader(400)
-			} else {
-				w.WriteHeader(500)
-			}
-			return
+func handleWebHook(w http.ResponseWriter, r *http.Request) {
+	events, err := bot.ParseRequest(r)
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
 		}
-		for _, event := range events {
-			if event.Type == linebot.EventTypeMessage {
-				replyToken := event.ReplyToken
-				userID := event.Source.UserID
-				groupID := event.Source.GroupID
-				RoomID := event.Source.RoomID
-				fmt.Printf("replyToken:%s\nuserID:%s\ngroupID:%s\nRoomID:%s", replyToken, userID, groupID, RoomID)
-
-				switch message := event.Message.(type) {
-				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				case *linebot.StickerMessage:
-					replyMessage := fmt.Sprintf(
-						"Image id is %s", message.ID)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-						log.Print(err)
-					}
-				case *linebot.ImageMessage:
-					replyMessage := fmt.Sprintf(
-						"sticOriginalContentURLker  is %s, OriginalContentURL is %s", message.PreviewImageURL, message.PreviewImageURL)
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
-						log.Print(err)
-					}
-				}
-
-			}
-		}
-	})
-
-	// Determine port for HTTP service.
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("defaulting to port %s", port)
+		return
 	}
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			replyToken := event.ReplyToken
+			userID := event.Source.UserID
+			groupID := event.Source.GroupID
+			RoomID := event.Source.RoomID
+			fmt.Printf("replyToken:%s\nuserID:%s\ngroupID:%s\nRoomID:%s", replyToken, userID, groupID, RoomID)
 
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Print(err)
+				}
+			case *linebot.StickerMessage:
+				replyMessage := fmt.Sprintf(
+					"Image id is %s", message.ID)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Print(err)
+				}
+			case *linebot.ImageMessage:
+				replyMessage := fmt.Sprintf(
+					"sticOriginalContentURLker  is %s, OriginalContentURL is %s", message.PreviewImageURL, message.PreviewImageURL)
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
+					log.Print(err)
+				}
+			}
+
+		}
 	}
 }
